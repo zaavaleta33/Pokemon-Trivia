c382e4278f69b5976ef3e2d6e832184a936b0076
zaavaleta33 <A_zavaleta@csu.fullerton.edu>
2023-11-17 14:27:47 -0800

    triviaupdate

--- poke/TriviaScreen.js
+++ poke/TriviaScreen.js
@@ -1,68 +1,81 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, Image, StyleSheet } from 'react-native';
+import { View, Text, Button, Image, StyleSheet, ScrollView } from 'react-native';
 import axios from 'axios';
 
 const TriviaScreen = ({ navigation }) => {
-    const [question, setQuestion] = useState({});
+  const [question, setQuestion] = useState({});
   const [userScore, setUserScore] = useState(0);
+
+  const types = [
+    'Normal', 'Fire', 'Water', 'Electric',
+    'Grass', 'Ice', 'Fighting', 'Poison',
+    'Ground', 'Flying', 'Psychic', 'Bug',
+    'Rock', 'Ghost', 'Dragon', 'Dark',
+    'Steel', 'Fairy'
+  ];
 
   useEffect(() => {
     fetchQuestion();
   }, []);
 
-  const fetchQuestion = async (callback) => {
-      try {
-        const response = await axios.get('https://pokeapi.co/api/v2/pokemon/1/');
-        const pokemonData = response.data;
-        const questionText = `What is the type of ${pokemonData.name}?`;
-        const correctAnswer = pokemonData.types[0].type.name;
-        setQuestion({ text: questionText, correctAnswer });
-        if (callback) {
-          callback(correctAnswer);
-        }
-      } catch (error) {
-        console.error('Error fetching question:', error);
-      }
-    };
-  const handleAnswer = (userAnswer) => {
-    if (userAnswer === question.correctAnswer) {
-      setUserScore(userScore + 1);
-      fetchQuestion();
-    } else {
-      navigation.navigate('LeaderScreen', { score: userScore });
+  const fetchQuestion = async () => {
+    try {
+      const randomPokemonId = Math.floor(Math.random() * 151) + 1;
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}/`);
+      const pokemonData = response.data;
+      const questionText = `What is the type of ${pokemonData.name}?`;
+      const correctAnswer = pokemonData.types[0].type.name;
+      setQuestion({ text: questionText, correctAnswer });
+    } catch (error) {
+      console.error('Error fetching question:', error);
     }
   };
 
+  const handleAnswer = (userAnswer) => {
+    if (userAnswer === question.correctAnswer) {
+      setUserScore(userScore + 1);
+    }
+    fetchQuestion();
+  };
+
   return (
-    <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', backgroundColor: '#A1C084', paddingTop: 32 }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-        <Image source={require('/Users/alexanderzavaleta/Desktop/Code/Mob411/Pokemon-Trivia/poke/assets/mew.png')}style={styles.imageLeft}  />
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.leaderboardContainer}>
+        <Image source={require('/Users/alexanderzavaleta/Desktop/Code/Mob411/Pokemon-Trivia/poke/assets/mew.png')} style={styles.imageLeft} />
         <Text style={styles.leaderboardText}>Leaderboard</Text>
         <Image source={require('/Users/alexanderzavaleta/Desktop/Code/Mob411/Pokemon-Trivia/poke/assets/mew.png')} style={styles.imageRight} />
       </View>
-      <Text>{question.text}</Text>
-      <Button title="Fire" onPress={() => handleAnswer('fire')} />
-      <Button title="Water" onPress={() => handleAnswer('water')} />
-      <Button title="Grass" onPress={() => handleAnswer('grass')} />
-    </View>
+      <Text style={styles.questionText}>{question.text}</Text>
+      <View style={styles.buttonContainer}>
+        {types.map(type => (
+          <Button key={type} title={type} onPress={() => handleAnswer(type.toLowerCase())} />
+        ))}
+      </View>
+      <Text style={styles.scoreText}>Score: {userScore}</Text>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flexGrow: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#A1C084',
+    paddingTop: 32,
+  },
+  leaderboardContainer: {
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#A1C084', 
-    padding: 16,
   },
- imageLeft: {
-    width: 50,
-    height: 50,
+  imageLeft: {
+    width: 100,
+    height: 100,
     resizeMode: 'contain',
     marginHorizontal: 8,
     transform: [{ scaleX: -1 }],
-
   },
   leaderboardText: {
     fontSize: 20,
@@ -70,11 +83,27 @@ const styles = StyleSheet.create({
     marginHorizontal: 8,
   },
   imageRight: {
-    width: 50,
-    height: 50,
+    width: 100,
+    height: 100,
     resizeMode: 'contain',
     marginHorizontal: 8,
-  },});
+  },
+  questionText: {
+    fontSize: 18,
+    marginVertical: 16,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    flexWrap: 'wrap',
+    width: '100%',
+    paddingHorizontal: 16,
+  },
+  scoreText: {
+    fontSize: 18,
+    marginVertical: 16,
+  },
+});
 
 export default TriviaScreen;
 
